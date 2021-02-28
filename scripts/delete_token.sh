27931cd6aaf74f21f61a62d58017239106a12654
Deepak Kumar <deepak.kumar@finoit.co.in>
2021-02-28 15:41:08 +0530

    cron

--- scripts/delete_token.sh
+++ scripts/delete_token.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-cd /home/ubuntu/spark_18/spark_18/
-source env/bin/activate
-python manage.py delete_token
-exit
+
+while [ true ]; do
+	sleep 1
+	# do what you need to here
+	cd /home/ubuntu/spark_18/spark_18/
+	source env/bin/activate
+	python manage.py delete_token
+	exit
+done
+
+
