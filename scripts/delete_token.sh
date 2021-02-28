708eef0ad40b15ac0de14086d95370a3e2903537
Deepak Kumar <deepak.kumar@finoit.co.in>
2021-02-28 15:58:59 +0530

    change script

--- scripts/delete_token.sh
+++ scripts/delete_token.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 while [ true ]; do
-	sleep 1
 	# do what you need to here
 	cd /home/ubuntu/spark_18/spark_18/
 	source env/bin/activate
 	python manage.py delete_token
-	exit
+	sleep 1
 done
 
 
